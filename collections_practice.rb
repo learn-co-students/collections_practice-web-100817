59f0c27db0a3a2f960f60b89900f32da555c3f80
github username <github email address>
2017-10-09 20:11:10 -0400

    again

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,47 +1,47 @@
-def sort_array_asc(arr)
-  arr.sort
+def sort_array_asc(array)
+  array.sort
 end
 
-def sort_array_desc(arr)
-  arr.sort do |x, y|
+def sort_array_desc(array)
+  array.sort do |x, y|
     y <=> x
   end
 end
 
-def sort_array_char_count(arr)
-  arr.sort do |x, y|
+def sort_array_char_count(array)
+  array.sort do |x, y|
     x.length <=> y.length
   end
 end
 
-def swap_elements(arr)
-  arr.insert(1, arr.delete_at(2))
+def swap_elements(array)
+  array.insert(1, array.delete_at(2))
 end
 
-def reverse_array(arr)
-  arr.reverse
+def reverse_array(array)
+  array.reverse
 end
 
-def kesha_maker(arr)
-  arr.each do |word|
+def kesha_maker(array)
+  array.each do |word|
     word[2] = "$"
   end
 end
 
-def find_a(arr)
-  arr.select do |word|
+def find_a(array)
+  array.select do |word|
     word[0] == "a"
   end
 end
 
-def sum_array(arr)
-  arr.inject do |sum, n|
+def sum_array(array)
+  array.inject do |sum, n|
     sum + n
   end
 end
 
-def add_s(arr)
-  arr.each_with_index.collect do |word, index|
+def add_s(array)
+  array.each_with_index.collect do |word, index|
     if index == 1
       word
     else
